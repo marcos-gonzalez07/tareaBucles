eb564916d4194fa85b98f67448c99f44daff63d1
marcos <marquitosgonzalez422@gmail.com>
2025-09-10 22:52:33 -0300

    realice script que calcula el valor de la letra de un dni

---
+++ js/05-ejercicio.js
@@ -0,0 +1,62 @@
+const dni = parseInt(prompt("ingrese su dni"))
+let continuar = true
+
+do{
+    if(isNaN(dni)){
+        alert("el valor ingresado no es un número")
+    } else if ( dni < 0 || dni > 99999999){
+        alert("el número debe estar entre 0 y 99.999.999")
+    } else{
+        let resto = dni % 23
+        let letra = ""
+        switch(resto){
+            case 0: letra ="T"
+                break
+            case 1: letra ="R"
+                break
+            case 2: letra ="W"
+                break
+            case 3: letra ="A"
+                break
+            case 4: letra ="G"
+                break
+            case 5: letra ="M"
+                break
+            case 6: letra ="Y"
+                break
+            case 7: letra ="F"
+                break
+            case 8: letra ="P"
+                break
+            case 9: letra ="D"
+                break
+            case 10: letra ="X"
+                break
+            case 11: letra ="B"
+                break
+            case 12: letra ="N"
+                break
+            case 13: letra ="J"
+                break
+            case 14: letra ="Z"
+                break
+            case 15: letra ="S"
+                break
+            case 16: letra ="Q"
+                break
+            case 17: letra ="V"
+                break
+            case 18: letra ="H"
+                break
+            case 19: letra ="L"
+                break
+            case 20: letra ="C"
+                break
+            case 21: letra ="K"
+                break
+            case 22: letra ="E"
+                break
+        }
+        document.writeln(`el dni ${dni} tiene la letra: ${letra}`)
+    }
+}while(continuar)
